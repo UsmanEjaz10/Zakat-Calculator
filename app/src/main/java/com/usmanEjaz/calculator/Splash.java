505a797e3478edab533f5ff41292e0c9e9bd8bd5
Usman <alie25708@gmail.com>
2023-04-15 19:10:20 +0500

    Hide action bar from splash screen

---
+++ app/src/main/java/com/usmanEjaz/calculator/Splash.java
@@ -0,0 +1,34 @@
+package com.usmanEjaz.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(Splash.this, MainActivity.class);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
